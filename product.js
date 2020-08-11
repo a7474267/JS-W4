888ea2e125c9bf7a9e3eb8a4ab8ebf963c488bb3
a7474267 <a7474267@gmail.com>
2020-08-12 00:29:12 +0800

    fix code

--- product.js
+++ product.js
@@ -38,46 +38,77 @@ var app = new Vue({
         },
         //刪除確認事件
         deleteProduct() {
+            //使用這個方式，只能在畫面上刪除資料，但實際上資料還是存在於遠端的伺服器內，並沒有真正做到刪除
+            // const vm = this;
+            // if (vm.tempProduct.id) {
+            //     const id = vm.tempProduct.id;
+            //     vm.product.forEach(function (item, index) {
+            //         if (item.id === id) {
+            //             vm.product.splice(index, 1);
+            //             vm.tempProduct = {};
+            //         }
+            //     })
+            // }
+            // $('#deleteModal').modal('hide');
             const vm = this;
-            if (vm.tempProduct.id) {
-                const id = vm.tempProduct.id;
-                vm.product.forEach(function (item, index) {
-                    if (item.id === id) {
-                        vm.product.splice(index, 1);
-                        vm.tempProduct = {};
-                    }
-                })
-            }
-            $('#deleteModal').modal('hide');
+            const url = `https://course-ec-api.hexschool.io/api/${vm.user.uuid}/admin/ec/product/${vm.tempProduct.id}`;
+            // 預設帶入 token
+            axios.defaults.headers.common.Authorization = `Bearer ${vm.user.token}`;
+            axios.delete(url)
+                .then(() => {
+                    $('#deleteModal').modal('hide'); // 刪除成功後關閉 Modal
+                    vm.getList(); // 重新取得全部資料
+                });
         },
         //新增or編輯確認事件
         editOrUpdate() {
+            //使用這個方式，只能在畫面上新增或編輯資料，但實際上資料沒有存到位於遠端的伺服器內
+            // const vm = this;
+            // //如果id為真值，代表是編輯產品
+            // if (vm.tempProduct.id) {
+            //     const id = vm.tempProduct.id;
+            //     vm.product.forEach(function (item, index) {
+            //         //使用id當做比對的標的物，對id相符的才做更新
+            //         if (item.id === id) {
+            //             vm.product[index] = vm.tempProduct;
+            //         }
+            //     });
+            // } //else代表新增產品 
+            // else {
+            //     const id = new Date().getTime();
+            //     //因為表單內沒有新增id的地方，這裡可以先把id push進去array
+            //     vm.tempProduct.id = id;
+            //     vm.product.push(vm.tempProduct)
+            // }
+            // vm.tempProduct = {};
+            // //清空暫時資料，然後關閉modal
+            // $('#productModal').modal('hide');
             const vm = this;
-            //如果id為真值，代表是編輯產品
+            const editUrl = `https://course-ec-api.hexschool.io/api/${vm.user.uuid}/admin/ec/product/${vm.tempProduct.id}`;
+            const addUrl = `https://course-ec-api.hexschool.io/api/${vm.user.uuid}/admin/ec/product`;
+            // 預設帶入 token
+            axios.defaults.headers.common.Authorization = `Bearer ${vm.user.token}`;
+            //如果id為真值，代表是編輯產品，使用patch覆蓋伺服器內的資料
             if (vm.tempProduct.id) {
-                const id = vm.tempProduct.id;
-                vm.product.forEach(function (item, index) {
-                    //使用id當做比對的標的物，對id相符的才做更新
-                    if (item.id === id) {
-                        vm.product[index] = vm.tempProduct;
-                    }
-                });
-            } //else代表新增產品 
-            else {
-                const id = new Date().getTime();
-                //因為表單內沒有新增id的地方，這裡可以先把id push進去array
-                vm.tempProduct.id = id;
-                vm.product.push(vm.tempProduct)
+                axios.patch(editUrl)
+                    .then(function () {
+                        $('#productModal').modal('hide'); // AJAX 新增成功後關閉 Modal
+                        vm.getList();//重新呈現產品列表
+                    })
             }
-            vm.tempProduct = {};
-            //清空暫時資料，然後關閉modal
-            $('#productModal').modal('hide');
+            else {
+                axios.post(addUrl)
+                    .then(function () {
+                        $('#productModal').modal('hide'); // AJAX 新增成功後關閉 Modal
+                        vm.getList();//重新呈現產品列表
+                    })
+            };
         },
-        //取得產品資料
+        //進入頁面後立刻載入現有的產品列表
         getList(num) {
             const vm = this;
             //為了避免num出現undefined的情形，於是預設num=1
-            if(!num){num=1};
+            if (!num) { num = 1 };
             //為了在網址顯示頁數，於是在url內增加 ?page=${num}, num 的參數由外面傳入
             const url = `https://course-ec-api.hexschool.io/api/${this.user.uuid}/admin/ec/products?page=${num}`;
             axios.get(url)
